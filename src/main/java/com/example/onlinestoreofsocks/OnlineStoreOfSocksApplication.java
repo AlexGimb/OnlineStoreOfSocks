f1f9b82b961ddb15bb4276b6e018a056dae9dd09
Alex <gimberg_aleksandr@mail.ru>
2023-01-11 16:56:06 +0700

    Initial commit

---
+++ src/main/java/com/example/onlinestoreofsocks/OnlineStoreOfSocksApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinestoreofsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreOfSocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreOfSocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinestoreofsocks/OnlineStoreOfSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinestoreofsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreOfSocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
